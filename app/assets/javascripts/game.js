a7cb5e4f0311587153446b798bd9e0ee14fadb66
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-04-09 00:00:59 -0600

    functioning button, styling and routes

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
-  var random_num = Math.floor(Math.random()*11),
-      random_num_2 = Math.floor(Math.random()*random_num),
-      operators = ["+", "-", "*"];
+  // var random_num = Math.floor(Math.random()*11),
+  //     random_num_2 = Math.floor(Math.random()*random_num),
+  //     operators = ["+", "-", "*"];
+  //
+  // var randomOperator = function () {
+  //   return operators[Math.floor(Math.random()*operators.length)];
+  // }
 
-  var randomOperator = function () {
-    return operators[Math.floor(Math.random()*operators.length)];
-  }
+  // var numberOne = Math.round(random_num),
+  //     numberTwo = Math.round(random_num_2),
+  //     operator = randomOperator();
 
-  var numberOne = Math.round(random_num),
-      numberTwo = Math.round(random_num_2),
-      operator = randomOperator();
+  var questionsArray = JSON.parse($(".questions-array").attr("value"))
+  var randomQuestion = questionsArray[Math.floor(Math.random()*questionsArray.length)];
 
-  $(".number-one").text(numberOne)
-  $(".number-two").text(numberTwo)
-  $(".property").text(operator)
+  $(".number-one").text(randomQuestion.number_1)
+  $(".number-two").text(randomQuestion.number_2)
+  $(".property").text(randomQuestion.operator)
 
   $("#form-answer").submit(function(event) {
     var answer = $("#answer").val(),
-        correctAnswer = eval(String(numberOne) + String(operator) + String(numberTwo));
+        correctAnswer = eval(String(randomQuestion.number_1) + String(randomQuestion.operator) + String(randomQuestion.number_2));
 
     if (correctAnswer === parseInt(answer)) {
       $(".container").html("<div class='answer-status bg-correct'><a href='#' onclick='location.reload();'>Correct! Next Question</a></div>")
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'games#new'
+
+  resources :games
 end
